41dbb0956b43caa6a4db6c96001d3a5264f7aec5
huykingsofm <huykingsofm@gmail.com>
2024-10-27 21:49:33 +0700

    update x/jwt + change infras type

--- usecase/abstraction/repository.go
+++ usecase/abstraction/repository.go
@@ -6,11 +6,12 @@ import (
 	"github.com/todennus/shared/enumdef"
 	"github.com/todennus/user-service/domain"
 	"github.com/todennus/x/enum"
+	"github.com/xybor-x/snowflake"
 )
 
 type UserRepository interface {
 	Create(ctx context.Context, user *domain.User) error
-	GetByID(ctx context.Context, userID int64) (*domain.User, error)
+	GetByID(ctx context.Context, userID snowflake.ID) (*domain.User, error)
 	GetByUsername(ctx context.Context, username string) (*domain.User, error)
 	CountByRole(ctx context.Context, role enum.Enum[enumdef.UserRole]) (int64, error)
 }
